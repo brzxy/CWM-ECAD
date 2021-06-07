9636cf7fa4e978b3a187dce686877d6c91b60d93
Cloud User <centos@ip-172-31-13-190.eu-west-2.compute.internal>
2021-06-07 13:41:17 +0000

    A complete solution to Ex2

--- Ex2/top.v
+++ Ex2/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #2 - Doorbell Chime
-// Student Name:
-// Date: 
+// Student Name:Xuyang Zhao
+// Date: 2021/6/7
 //
 //  Description: In this exercise, you need to design a multiplexer that chooses between two sounds, where the  
 //  output is delayed by 5 ticks (not clocks!) and acts according to the following truth table:
@@ -22,11 +22,17 @@
 
 module doorbell(
     //Todo: define inputs here
+    input a,
+    input b,
+    input [2:0] sel,
+    output out
     );
     
+    
     //Todo: define registers and wires here
-
+    wire   out;
 
     //Todo: define your logic here                 
-      
+      assign #10 out=(sel==0)?a:
+                     b;   //out=b when sel=1
 endmodule
