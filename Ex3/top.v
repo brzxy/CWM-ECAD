30fdd83dee9dd6bb7df93d5607bcff6698bfe53c
Cloud User <centos@ip-172-31-13-190.eu-west-2.compute.internal>
2021-06-07 15:36:35 +0000

    attempt to Ex3

--- Ex3/top.v
+++ Ex3/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #3 - Active IoT Devices Monitor
-// Student Name:
-// Date: 
+// Student Name:Xuyang Zhao
+// Date: 2021/6/7
 //
 //  Description: In this exercise, you need to design a counter of active IoT devices, where 
 //  if the rst=1, the counter should be set to zero. If event=0, the value
@@ -20,11 +20,31 @@
 
 module monitor (
     //Todo: add ports 
-
+    input clk,
+    input rst,
+    input change,   
+    input on_off,     //four inputs
+    output [7:0] counter_out         //output out
     );
                     
     //Todo: add registers and wires, if needed
-
+    reg [7:0] counter_out;   //set the output as register as we want it to store information
+    
     //Todo: add user logic
-      
+    always @(posedge clk)
+     begin
+
+        if(rst)
+          counter_out <= 0;  //if reset then output=o
+        else  
+          begin
+  
+            if(change)       //so if change=0,output stays constant                
+                if(on_off)   //if on then count up
+                  counter_out<=counter_out+1;
+                else         //if off then count down
+                  counter_out<=counter_out-1;
+              
+          end
+     end
 endmodule
--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #3 - Active IoT Devices Monitor
-// Student Name:
-// Date: 
+// Student Name:Xuyang Zhao
+// Date: 2021/6/7
 //
 // Description: A testbench module to test Ex3 - Active IoT Devices Monitor
 // Guidance: start with simple tests of the module (how should it react to each 
@@ -14,15 +14,36 @@ module top_tb(
     );
     
 //Todo: Parameters
-
+parameter CLK_PERIOD = 10; //set the clock period
 //Todo: Regitsers and wires
+reg clk, rst, data, data_out;
+
 
 //Todo: Clock generation
 
+   initial begin //This block generates the clock with a period of 10 time units
+      clk=0;
+      forever
+      #(CLK_PERIOD/2) clk=~clk; //happens every delay of CLK_PERIOD/2
+   end
+
 //Todo: User logic
-    
+
+   initial begin //This block drives inputs to user’s module and checks output
+      rst=1;
+      data=0;
+      #10 rst=0;
+      data=1;
+      #10
+      $display(“data= %h, data_out=%h”, data, data_out);
+   end    
 //Todo: Finish test, check for success
 
 //Todo: Instantiate counter module
- 
+   monitor top(
+     .a (ab[1]),
+     .b (ab[0]),
+     .sel (sel),
+     .out (out)
+     );
 endmodule 
