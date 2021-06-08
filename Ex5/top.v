39280207294c6f1f9a955e00212c707eeb5ded72
Cloud User <centos@ip-172-31-13-190.eu-west-2.compute.internal>
2021-06-08 15:40:55 +0000

    A trial solution to Ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name:Xuyang Zhao
+// Date: 2021/6/8
 //
 //  Description: In this exercise, you need to an air conditioning control system
 //  According to the state diagram provided in the exercise.
@@ -12,4 +12,79 @@
 //  outputs:
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module AIR (
+    //add ports 
+    input clk,  
+    input [4:0] temperature,     //two inputs
+    output heating,
+    output cooling         //two outputs 
+    );
+                    
+    //add registers and wires, if needed
+    reg heating;
+    reg cooling;
+
+    //add user logic
+    //{000} and {111} are not used, for other colours, change if button=1, stay constant if button=0
+    always @(posedge clk)
+     begin
+        
+
+        //for the state to be COOLING ON
+        if((heating==0)&&(cooling==1))
+        begin
+            if(temperature<=20)
+            begin 
+               cooling<=0; 
+               heating<=0;     //turn off cooling and turn off heating
+            end
+            else
+            begin 
+               cooling<=1; 
+               heating<=0;     //turn on cooling and turn off heating
+            end
+        end
+
+
+        //for the state to be HEATER ON
+        if((heating==1)&&(cooling==0))
+        begin
+            if(temperature>=20)
+            begin 
+               cooling<=0; 
+               heating<=0;     //turn off cooling and turn off heating
+            end
+            else
+            begin 
+               cooling<=0; 
+               heating<=1;     //turn off cooling and turn on heating
+            end
+        end
+
+
+        //for the state to be IDLE
+        if((heating==0)&&(cooling==0))
+        begin
+            if(temperature>=22)
+            begin 
+               cooling<=1; 
+               heating<=0;     //turn on cooling and turn off heating
+            end
+
+            if(temperature<=18)
+            begin 
+               cooling<=0;
+               heating<=1;     //turn off cooling and turn on heating
+            end
+            
+            else begin
+               cooling<=0;
+               heating<=0;     //turn off cooling and turn off heating
+            end
+        end
+ 
+    end
+
+endmodule
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,91 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name:Xuyang Zhao
+// Date: 2021/6/8
 //
 // Description: A testbench module to test Ex5 - Air Conditioning
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+    );
+    
+// Parameters
+parameter CLK_PERIOD = 10; //set the clock period
+//Todo: Regitsers and wires
+reg clk;
+reg [4:0] temperature;
+reg err;
+reg cooling;
+reg heating;
+
+//Todo: Clock generation
+
+   initial
+   begin //This block generates the clock with a period of 10 time units
+      clk=0;
+      forever
+        #(CLK_PERIOD/2) clk=~clk; //happens every delay of CLK_PERIOD/2
+      end
+
+//Todo: User logic
+
+   initial 
+   begin //This block drives inputs to user’s module and checks output
+        err=0;         
+        cooling=0;
+        heating=0;
+        temperature=0;//set the initial temperature to be zero   
+
+        
+        //check for HEATING ON state
+        if ((cooling==0)&&(heating!=1))
+        //for temperature being zero the heating should be 1 
+        begin
+           $display("TEST FAILED");//if not then test failed
+            err = 1;
+        end
+        
+        temperature=temperature+21;
+        //check for IDLE state
+        if ((cooling==0)&&(heating!=0))
+        //for temperature being 21 the heating should be 0
+        begin
+           $display("Test FAILED");//if not then test failed
+           err = 1;
+        end
+
+
+        temperature=temperature+1;
+        //check for COOLING ON state
+        if ((heating==0)&&(cooling!=1))
+        //for temperature being 22 the cooling should be 1
+        begin
+           $display("Test FAILED");//if not then test failed
+           err = 1;
+        end
+
+   end  
+  
+//Todo: Finish test, check for success
+
+     initial
+     begin
+        #50
+        if (err==0)
+          $display("TEST PASSED!");
+        $finish;
+     end
+
+//Todo: Instantiate module
+   
+   AIR top(
+     .clk (clk),
+     .temperature (temperature),
+     .heating (heating),
+     .cooling (cooling)
+   );
+
+
+endmodule 
